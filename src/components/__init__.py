508c177569f122dd0ae123d34541dc91de2b64ac
suryansh <suryanshnema25@gmail.com>
2023-11-13 15:49:03 +0530

    first commit

---
+++ src/components/__init__.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="suryansh",
+    author_email="suryanshnema03@gmail.com",
+    packages=find_packages(),
+    install_requires=['pandas'],
+)
+
